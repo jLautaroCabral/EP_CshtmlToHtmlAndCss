97a193d1aabe632b2e47fb5075e6f329dbe72016
Lautaro Cabral <lc3027@outlook.com.ar>
2020-09-07 07:40:52 -0300

    Esta vez si es el ultimo commit

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Repositorio del de la clase 3 del programa CLIP
 
 Como justo estoy estudiando ASP quise hacer la entrega con ASP, de todas formas entiendo que quizás sólo querían ver HTML, CSS y nada más.
-Por eso mismo hice un pequeño programa adicional que traduzca el proyecto ASP a sólo unos archivos HTML y CSS, sin embargo tiene pequeños defectos aún :(
+Por eso mismo hice un pequeño programa adicional que traduzca el proyecto ASP a sólo unos archivos HTML y CSS
 
-Puedes ejecutar el traductor para probar mi intento de traducción
+Puedes ejecutar el traductor para probar la traducción y ver en limpio el HTML y CSS generado
+
+El código del traductor está en la carpeta traductor-project
+El código del proyecto ASP está en la carpeta asp-project
+
+El código del traductor está comentado!
--- traducor-project/traductor/Program.cs
+++ traducor-project/traductor/Program.cs
@@ -8,14 +8,25 @@ namespace traductor
     {
         static void Main(string[] args)
         {
+
+            // ------------------------------------------------------------------
+            // ------------------------------------------------------------------
+
+
+            // Se establecen las rutas a utilizar
             Console.WriteLine("Comenzando la traducción...");
-
             string pathAspProject = @"../../../../../asp-project";
-
             string pathResultado = @"../../../../../resultado_traductor";
+
             string pathCss = Path.Combine(pathResultado, "css");
             string pathAssets = Path.Combine(pathResultado, "assets");
 
+
+            // ------------------------------------------------------------------
+            // ------------------------------------------------------------------
+
+
+            // Se crean directorios utilizando algunas de las anteriores rutas
             Console.WriteLine("Creando directorios...");
             if (!File.Exists(pathResultado))
             {
@@ -25,10 +36,16 @@ namespace traductor
                 Console.WriteLine("La carpeta resultado_traductor ha sido creada exitosamente!");
             }
 
+
+            // ------------------------------------------------------------------
+            // ------------------------------------------------------------------
+
+            // Se copian los archivos CSS y assets a la ruta resultado
             Console.WriteLine("Copiando ficheros...");
             File.Copy($"{pathAspProject}/wwwroot/css/site.css", $"{pathCss}/site.css", true);
             File.Copy($"{pathAspProject}/wwwroot/assets/cheems.png", $"{pathAssets}/cheems.png", true);
 
+            // Se cran los html en la ruta resultado
             Console.WriteLine("Creando ficheros...");
             FileStream fs;
             fs = File.Create($"{pathResultado}/index.html");
@@ -38,36 +55,30 @@ namespace traductor
             fs = File.Create($"{pathResultado}/enlaces.html");
             fs.Close();
 
+
+            // ------------------------------------------------------------------
+            // ------------------------------------------------------------------
+
+            // Se leen los archivos HTML y se guarda su contenido en variables
             Console.WriteLine("Recuperando HTML...");
             string layoutHtml = File.ReadAllText($"{pathAspProject}/Views/Shared/_Layout.cshtml");
             string indexHtml = File.ReadAllText($"{pathAspProject}/Views/Home/Index.cshtml");
             string enlacesHtml = File.ReadAllText($"{pathAspProject}/Views/Home/Enlaces.cshtml");
             string tecnologiasHtml = File.ReadAllText($"{pathAspProject}/Views/Home/Tecnologías.cshtml");
 
-            /*
-            public class Example
-        {
-            public static void Main()
-            {
-                string pattern = "(Mr\\.? |Mrs\\.? |Miss |Ms\\.? )";
-                string[] names = { "Mr. Henry Hunt", "Ms. Sara Samuels",
-                         "Abraham Adams", "Ms. Nicole Norris" };
-                foreach (string name in names)
-                    Console.WriteLine(Regex.Replace(name, pattern, String.Empty));
-            }
-        }
-        // The example displays the following output:
-        //    Henry Hunt
-        //    Sara Samuels
-        //    Abraham Adams
-        //    Nicole Norris
 
-            */
+            // ------------------------------------------------------------------
+            // ------------------------------------------------------------------
 
-
-        Console.WriteLine("Traduciendo HTML...");
-
+            // Utilizando Expresiones Regulares se crean nuevos strings juntando el HTML del Layout y el HTML de las otras vistas,
+            // de la misma manera se reemplazan las directivas de ASP por HTML puro. Luego se guardan en variables strings
+            // Las variables de esta sección que empiezan con "p", son los patrones que se buscan al usar RegEx
+            Console.WriteLine("Traduciendo HTML...");
             string pRenderBody = @"@RenderBody\(\)";
+
+            // Esta salsa se produjo al tratar de "escapar" carácteres de strings y al mismo tiempo tratar de "escapar" carácteres de RegEx
+            string pRenderScripts = @"@RenderSection\(" + $"\"Scripts\"" + @", required: false\)"; 
+
             string pLinkCss = "~/css/site.css";
             string pCheems = "~/assets/cheems.png";
             string pAspController = $"asp-controller=\"Home\"";
@@ -75,32 +86,38 @@ namespace traductor
             string pAspActionT = $"asp-action=\"Tecnologías\"";
             string pAspActionE = $"asp-action=\"Enlaces\"";
 
-            Console.WriteLine(pAspController);
-
             string newLayoutHtml;
             string newIndexHtml;
             string newEnlacesHtml;
             string newTecnologíasHtml;
 
-            newLayoutHtml = Regex.Replace(layoutHtml, pLinkCss, "/css/site.css");
+            newLayoutHtml = Regex.Replace(layoutHtml, pLinkCss, "css/site.css");
             newLayoutHtml = Regex.Replace(newLayoutHtml, pAspController, String.Empty);
+            newLayoutHtml = Regex.Replace(newLayoutHtml, pRenderScripts, String.Empty); 
             newLayoutHtml = Regex.Replace(newLayoutHtml, pAspActionI, "href=\"./index.html\"");
             newLayoutHtml = Regex.Replace(newLayoutHtml, pAspActionT, "href=\"./tecnologias.html\"");
             newLayoutHtml = Regex.Replace(newLayoutHtml, pAspActionE, "href=\"./enlaces.html\"");
 
             newIndexHtml = Regex.Replace(newLayoutHtml, pRenderBody, indexHtml);
-            newIndexHtml = Regex.Replace(newIndexHtml, pCheems, "/assets/cheems.png");
+            newIndexHtml = Regex.Replace(newIndexHtml, pCheems, "assets/cheems.png");
 
             newEnlacesHtml = Regex.Replace(newLayoutHtml, pRenderBody, enlacesHtml);
             newTecnologíasHtml = Regex.Replace(newLayoutHtml, pRenderBody, tecnologiasHtml);
 
+            // ------------------------------------------------------------------
+            // ------------------------------------------------------------------
+             
+            // Se escribe en los HTML creados anteriormente el contenido de las variables con el HTML ya procesado
             Console.WriteLine("Generando nuevo HTML...");
             File.WriteAllText($"{pathResultado}/index.html", newIndexHtml);
             File.WriteAllText($"{pathResultado}/enlaces.html", newEnlacesHtml);
             File.WriteAllText($"{pathResultado}/tecnologias.html", newTecnologíasHtml);
 
             Console.WriteLine("La traducción se ha completado exitosamente!");
-            string i = Console.ReadLine();
+            Console.WriteLine("============================================");
+            Console.WriteLine("");
+            Console.WriteLine("Para volver a ejecutar la traduccion tienes que borrar la carpeta generada \"resultado_traductor\" ");
+            var i = Console.ReadKey();
         }
     }
 }
